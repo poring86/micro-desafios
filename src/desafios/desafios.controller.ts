57bbcdbf89b0baeca9aed4a82ad6b0b3dc4ec14c
Matheus Lino <matheuslino86a@gmail.com>
2022-09-15 16:02:14 -0400

    Criar desafior

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,4 +1,29 @@
 import { Controller } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { DesafiosService } from './desafios.service';
+import { Desafio } from './interfaces/desafio.interface';
+
+const ackErrors: string[] = ['E11000'];
 
 @Controller('desafios')
-export class DesafiosController {}
+export class DesafiosController {
+  constructor(private readonly desafiosService: DesafiosService) {}
+
+  @EventPattern('criar-desafio')
+  async criarDesafio(@Payload() desafio: Desafio, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      await this.desafiosService.criarDesafio(desafio);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      console.log(`error: ${JSON.stringify(error.message)}`);
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+      if (filterAckError.length > 0) {
+        await channel.ack(originalMsg);
+      }
+    }
+  }
+}
--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DesafioStatus } from './desafio-status.enum';
+import { Desafio } from './interfaces/desafio.interface';
 
 @Injectable()
-export class DesafiosService {}
+export class DesafiosService {
+  constructor(
+    @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
+  ) {}
+
+  async criarDesafio(desafio: Desafio): Promise<Desafio> {
+    try {
+      const desafioCriado = new this.desafioModel(desafio);
+      desafioCriado.dataHoraSolicitacao = new Date();
+      /*
+            Quando um desafio for criado, definimos o status 
+            desafio como pendente
+        */
+      desafioCriado.status = DesafioStatus.PENDENTE;
+      console.log(`desafioCriado: ${JSON.stringify(desafioCriado)}`);
+      return await desafioCriado.save();
+    } catch (error) {
+      console.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
+  }
+}
